983902f111a68b66f94c28f3da53738b1a05cfbd
Santiago Mitidiero <santimiti10@gmail.com>
2024-02-10 00:51:02 -0300

    feat: add dropdown and delete button

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 - [✅] add firebase firestore
 - [✅] API
 - [✅] redirect
-- [] dropdown menu
-- [] delete client-side logic
+- [✅] dropdown menu
+- [✅] delete client-side logic
 - [] agregar user id a firestore y filtrar las urls con eso
 - [] command palette
 - [] handle errors
--- src/components/dashboard/Card.tsx
+++ src/components/dashboard/Card.tsx
@@ -1,21 +1,61 @@
+import { useState } from "react";
+
 interface Props {
   slug: string;
   url: string;
-  id: string;
+  urlId: string;
   description: string;
 }
 
-const Card: React.FC = ({ slug, url, id, description }: Props) => {
+const Card: React.FC = ({ slug, url, urlId, description }: Props) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(`http://localhost:4321/s/${slug}`); //TODO: cambiar link
+  }
+
+  function deleteUrl() {
+    fetch(`/api/urls/delete/${urlId}`, {
+      method: "DELETE",
+    }).then(() => {
+      window.location.reload();
+    });
+  }
+
   return (
-    <div className="border border-slate-900 p-4 m-4 relative flex flex-col items-start justify-center">
-      <a href={`/s/${slug}`} className="text-xl font-semibold hover:underline mb-1">
+    <div className="border border-slate-900 p-4 m-4 relative flex flex-col items-start justify-center rounded">
+      <a
+        href={`/s/${slug}`}
+        className="text-xl font-semibold hover:underline mb-1"
+      >
         /s/{slug}
       </a>
       <span className="text-gray-400">{url}</span>
       <p className="text-gray-300">{description}</p>
-      <button className="text-gray-400 hover:text-white transition absolute top-1 right-6 text-3xl">
+      <button
+        className="text-gray-400 hover:text-white transition absolute top-1 right-6 text-3xl"
+        onClick={() => setShowMenu(!showMenu)}
+      >
         ...
       </button>
+      <div
+        className={`${
+          showMenu ? "block fade-in-dropdown" : "hidden fade-out-dropdown"
+        } flex flex-col bg-slate-900 absolute top-12 right-0 border border-slate-800 rounded-md px-1 py-2`}
+      >
+        <button
+          className="hover:bg-slate-700 py-2 px-6 w-full transition rounded-sm"
+          onClick={() => copyToClipboard()}
+        >
+          Copy
+        </button>
+        <button
+          className="hover:bg-slate-700 py-2 px-6 w-full transition rounded-sm"
+          onClick={() => deleteUrl()}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
